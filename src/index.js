d95cd6f03b6f8e778053ea95aa0dcbf2175d6b45
Fi1osof <n.lanets@modxclub.ru>
2018-10-09 09:36:50 +0300

    1.0.0 dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export default 'Welcome to @prisma-cms/connector'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class PrismaCmsConnector extends Component {
+
+  
+  static contextTypes = {
+    getQueryFragment: PropTypes.func.isRequired,
+  }
+
+
+  shouldComponentUpdate(){
+    return false;
+  }
+
+
+  render() {
+
+
+    const {
+      ...other
+    } = this.props;
+  
+    const Query = this.prepareQuery();
+
+    return <Query 
+      {...other}
+    />;
+
+  }
+
+}
